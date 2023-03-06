ac5403dca3edb5de797ad468a6d87df2c11a789f
TuTDem <andreshernandez20001@gmail.com>
2023-03-06 12:58:51 -0500

    primera pregunta

--- src/examen.cpp
+++ src/examen.cpp
@@ -6,6 +6,19 @@
 #include <stdlib.h>
 using namespace std;
 
+//Constantes globales
+const string NLLNOMBRE = "Nahomy Nayeli Llumiquinga Cando";
+const string NLLCEDULA = "1725630204";
+
+struct nllcoordenada
+{
+    int  nllCap;
+    string  nllGeo;
+    string  nllArsenal;
+    ;
+    
+};
+
 int main()
 {
     return 0;
